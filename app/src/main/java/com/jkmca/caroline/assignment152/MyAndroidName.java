e038feadb38bb78caacb9906fea7183fac791810
carolineps777@gmail.com <38693976+Caroline-Sahrudayi@users.noreply.github.com>
2018-08-29 23:42:55 +0530

    material design assignment

---
+++ app/src/main/java/com/jkmca/caroline/assignment152/MyAndroidName.java
@@ -0,0 +1,15 @@
+package com.jkmca.caroline.assignment152;
+
+public class MyAndroidName {
+    private String androidName;
+
+    // constructor called which takes an argument of string type.
+    public MyAndroidName(String androidName) {
+        this.androidName = androidName;
+    }
+
+    // getter that returns a string.
+    public String getAndroidName() {
+        return androidName;
+    }
+}
